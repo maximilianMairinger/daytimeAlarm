236e3522eca1c18cdd3404309ecbcc823f4cae78
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-09-12 14:37:43 +0200

    repeat implemented

--- app/src/daytimeAlarm.ts
+++ app/src/daytimeAlarm.ts
@@ -1,8 +1,17 @@
 import { Data, DataCollection, DataSubscription } from "josm"
+import xtring from "xtring"; xtring()
 
 function currentDaytimeInMs() {
-  let d = new Date()
+  let d = now()
   return d.getHours() * toMs.hour + d.getMinutes() * toMs.minute + d.getSeconds() * toMs.second + d.getMilliseconds()
+}
+
+function nowInMs() {
+  return +now()
+}
+
+function now() {
+  return new Date
 }
 
 type Metric = ["hour", "minute", "second"]
@@ -39,13 +48,25 @@ function constructToHumanizedMetric(digits_metricName: number | string): (metric
   }
 }
 
-function splitDayTimeString(dayTime: string): { plain: { [key in Metric[number]]: Data<number> }, ms: { [key in Metric[number]]: Data<number> }, humanized: { [key in Metric[number]]: Data<string> } } {
-  let splitDayTime = dayTime.split(":")
+function formalizeGetMetric(metric: string) {
+  return `get${metric.capitalize()}s`
+}
+
+function splitDayTimeString(dayTime: string | Date): { plain: { [key in Metric[number]]: Data<number> }, ms: { [key in Metric[number]]: Data<number> }, humanized: { [key in Metric[number]]: Data<string> } } {
+  let splitDayTime: [hour: number, minute: number, second: number]
+  if (dayTime instanceof Date) {
+    splitDayTime = [] as any
+    for (let metric of metrics) {
+      splitDayTime.push(dayTime[formalizeGetMetric(metric)]())
+    }
+  }
+  else splitDayTime = dayTime.split(":").map(e => +e) as [hour: number, minute: number, second: number]
+  
   let ret = {plain: {}, ms: {}, humanized: {}}
   for (let i = 0; i < metrics.length; i++) {
     const metric = metrics[i]
     const toMsFactor = toMs[metric]
-    const pln = ret.plain[metric] = new Data(splitDayTime[i] ? +splitDayTime[i] : 0)
+    const pln = ret.plain[metric] = new Data(splitDayTime[i] ? splitDayTime[i] : 0)
     ret.ms[metric] = pln.tunnel(time => time * toMsFactor)
     ret.humanized[metric] = pln.tunnel(constructToHumanizedMetric(metric))
   }
@@ -53,6 +74,10 @@ function splitDayTimeString(dayTime: string): { plain: { [key in Metric[number]]
   return ret as any
 }
 
+
+const repeatAlias = {
+  daily: (now: Date) => now.setDate(now.getDate() + 1)
+}
 
 
 export class DayTimeAlarm {
@@ -66,7 +91,7 @@ export class DayTimeAlarm {
 
   private humanized: Data<string>
 
-  constructor(dayTime: string) {
+  constructor(dayTime: string | Date) {
     const { plain, ms: {hour, minute, second}, humanized: {hour: humHour, minute: humMin, second: humSec} } = splitDayTimeString(dayTime)
     for (let metric in plain) {
       this[metric] = plain[metric]
@@ -74,10 +99,10 @@ export class DayTimeAlarm {
     this.humanized = new Data
     new DataCollection(humHour, humMin, humSec).get((...a) => {
       let str = ""
-      for (let k of a) {
-        str += k + ":"
+      for (let i = 0; i < a.length - 1; i++) {
+        str += a[i] + ":"
       }
-      this.humanized.set(str.substring(0, str.length-1))
+      this.humanized.set(str + a[a.length - 1])
     })
 
     this.ms = new Data
@@ -86,15 +111,67 @@ export class DayTimeAlarm {
       this.ms.set(hourInMs + minuteInMs + secondInMs)
     })
 
-    new DataCollection(this.running, this.ms).get((running, absMs) => {
+    new DataCollection(this.running, this.ms, this.repeatOn).get((running, absMs, repeatOn) => {
       this.clearTimeout()
       if (running) {
-        let ms = absMs - currentDaytimeInMs()
-        if (ms < 0) ms += toMs.day
+        let ms: number
+        if (repeatOn === null) {
+          ms = absMs - currentDaytimeInMs()
+          if (ms < 0) ms += toMs.day
+        }
+        else {
+          ms = repeatOn - nowInMs() + absMs
+          if (ms < 0) {
+            let date = new Date(repeatOn)
+            console.warn(`DayTimeAlarm: Repeat: Given next day ${date.getDate()}.${date.getMonth()}.${date.getFullYear()} is in the past!?`)
+            return
+          }
+        }
+        
         this.timeoutID = setTimeout(this.alarmCallbacks.bind(this), ms)
       }
     }, false)
 
+  }
+
+  hasAlarmPassedToday() {
+    let n = now()
+    if (this.hour.get() < n.getHours()) return true
+    if (this.hour.get() === n.getHours()) {
+      if (this.minute.get() < n.getMinutes()) return true
+      if (this.minute.get() === n.getMinutes()) {
+        if (this.second.get() <= n.getSeconds()) return true
+      }
+    }
+    return false
+  }
+
+  private repeatFunction: Data<((now: Date) => (Date | unknown)) | null> = new Data(null)
+  private repeatFunctionExecuter = (func: null | ((now: Date) => (Date | unknown)), fromCurrentDay = false) => {
+    if (func) {
+      let d = now()
+      if (!fromCurrentDay) d.setDate(d.getDate() - 1)
+      let q = func(d)
+      if (q instanceof Date) d = q
+      d.setHours(0)
+      d.setMinutes(0)
+      d.setSeconds(0)
+      d.setMilliseconds(0)
+      return +d
+    }
+    else return null
+  }
+  private repeatOn: Data<number | null> = this.repeatFunction.tunnel(this.repeatFunctionExecuter)
+  
+  repeat(when: ((now: Date) => (Date | unknown)) | keyof typeof repeatAlias | null = repeatAlias.daily) {
+    this.repeatFunction.set(typeof when === "string" ? repeatAlias[when] : when)
+    return this as Omit<this, "repeat" | "start">
+  }
+  
+
+  once() {
+    this.repeat(null)
+    return this as Omit<this, "once" | "start">
   }
 
 
@@ -103,6 +180,7 @@ export class DayTimeAlarm {
     for (let cb of this.alarmCbs) {
       cb(s)
     }
+    this.repeatOn.set(this.repeatFunctionExecuter(this.repeatFunction.get(), true))
   }
   private alarmCbs = []
   onAlarm(cb: (toString: string) => void) {
@@ -122,7 +200,7 @@ export class DayTimeAlarm {
   }
   cancel() {
     this.running.set(false)
-    return this as Omit<this, "cancel">
+    return this as Omit<this, "cancel" | "once" | "repeat">
   }
   private clearTimeout() {
     if (this.timeoutID !== undefined) clearTimeout(this.timeoutID)
@@ -137,6 +215,6 @@ export class DayTimeAlarm {
   }
 }
 
-export default function(dayTime: string) {
+export default function(dayTime: string | Date) {
   return new DayTimeAlarm(dayTime).start()
 }
--- repl/src/repl.ts
+++ repl/src/repl.ts
@@ -1,6 +1,6 @@
 import daytimeAlarm from "./../../app/src/daytimeAlarm"
 //const testElem = document.querySelector("#test")
 
-daytimeAlarm("1:52").onAlarm((s) => {
-  console.log("aaaa", s)
-})
+daytimeAlarm("14:35:50").onAlarm((s) => {
+  console.log(s)
+}).repeat()
